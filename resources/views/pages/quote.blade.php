dfad01409e141dbc2110c5b7c605c0a8be6db4ea
Michael Joyner <joyner.michael@gmail.com>
2015-02-14 19:53:06 +0800

    js transform prefix all pages

--- resources/views/pages/quote.blade.php
+++ resources/views/pages/quote.blade.php
@@ -87,6 +87,7 @@
     function showTitle() {
         var title = document.querySelector(".products-title");
         title.style.transform = "rotateX(0deg)";
+        title.style.webkitTransform = "rotateX(0deg)";
     }
     window.addEventListener('load', showTitle, false);
 </script>
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -14,6 +14,7 @@
     function showTitle() {
         var title = document.querySelector(".products-title");
         title.style.transform = "rotateX(0deg)";
+        title.style.webkitTransform = "rotateX(0deg)";
     }
     window.addEventListener('load', showTitle, false);
     showTitle();
